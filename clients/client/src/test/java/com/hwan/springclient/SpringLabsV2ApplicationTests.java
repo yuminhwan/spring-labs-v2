d90547d281ff298d6833a34c6a8b5168968bfd7b
yuminhwan <ymh7532@icloud.com>
2023-09-11 23:23:58 +0900

    chore: 프로젝트 설정

---
+++ clients/client/src/test/java/com/hwan/springclient/SpringLabsV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwan.springclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLabsV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
