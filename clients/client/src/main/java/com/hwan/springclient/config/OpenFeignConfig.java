6e17dc2e68e423e1af02272d67e31937461abd89
yuminhwan <ymh7532@icloud.com>
2023-09-17 13:52:00 +0900

    chore: openFeign 타임아웃, 재시도, 로깅 설정

--- clients/client/src/main/java/com/hwan/springclient/config/OpenFeignConfig.java
+++ clients/client/src/main/java/com/hwan/springclient/config/OpenFeignConfig.java
@@ -1,9 +1,33 @@
 package com.hwan.springclient.config;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import feign.Logger;
+import feign.Retryer;
 
 @Configuration
 @EnableFeignClients("com.hwan.springclient")
 public class OpenFeignConfig {
+
+    /**
+     * 0.1초 간격으로 시작해 최대 3초의 간격으로 점점 증가하며, 최대 5번 재시도
+     * IOException이 발생한 경우에만 적용된다.
+     */
+    @Bean
+    Retryer.Default retryer() {
+        return new Retryer.Default(
+            100L,
+            TimeUnit.SECONDS.toMillis(3L),
+            5
+        );
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
 }
